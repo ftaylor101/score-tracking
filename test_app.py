62fb32d9835dead697104bf80b7ee6475b364433
ftaylor101 <45371775+ftaylor101@users.noreply.github.com>
2023-01-21 15:14:03 +0000

    More learning Firestore

--- test_app.py
+++ test_app.py
@@ -9,12 +9,16 @@ db = firestore.Client.from_service_account_json("firestore-key.json")
 doc_ref = db.collection("test_scores")
 
 # Get the data
-doc = doc_ref.get()
+# doc = doc_ref.get()
 
+st.write("## Query test")
 # print to screen
 for doc in doc_ref.stream():
-    st.write("The id is: ", doc.id)
-    st.write("The contents are:", doc.to_dict())
+    # st.write("The id is: ", doc.id)
+    doc_dict = doc.to_dict()
+    if "sprint1" in doc_dict.keys():
+        if doc_dict["sprint1"] > 10:
+            st.write(f"{doc.id} scored {doc_dict['sprint1']} points")
 
 # # Create new test_score document
 # doc_ref = db.collection("test_scores").document("player3_scores")
@@ -25,3 +29,8 @@ for doc in doc_ref.stream():
 #     "bonus30_mgp": 0
 # })
 
+st.write("## Setting test")
+players_ref = db.collection_group("Frederic")
+for pick in players_ref.stream():
+    st.write(pick.id)
+    st.write(pick.to_dict())
