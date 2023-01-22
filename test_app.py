f00ce20ea55e8ff8dada81a911df19176025414f
ftaylor101 <45371775+ftaylor101@users.noreply.github.com>
2023-01-22 20:13:10 +0000

    Simplifying test data structure to learn Firestore

--- test_app.py
+++ test_app.py
@@ -6,19 +6,28 @@ from google.cloud import firestore
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
 # Create a reference to the collection
+st.write("## Rider scores")
 doc_ref = db.collection("test_scores")
 
-# Get the data
-# doc = doc_ref.get()
-
-st.write("## Query test")
-# print to screen
+# Get data and print to screen
 for doc in doc_ref.stream():
-    # st.write("The id is: ", doc.id)
-    doc_dict = doc.to_dict()
-    if "sprint1" in doc_dict.keys():
-        if doc_dict["sprint1"] > 10:
-            st.write(f"{doc.id} scored {doc_dict['sprint1']} points")
+    st.write(f"{doc.id} has details: {doc.to_dict()} points")
+
+
+st.write("## Player picks")
+doc_ref = db.collection("Picks")
+
+# Get data and print to screen
+for doc in doc_ref.stream():
+    st.write(f"{doc.id} has picked: {doc.to_dict()}")
+
+
+st.write("## Player scores")
+doc_ref = db.collection("Players")
+
+# Get data and print to screen
+for doc in doc_ref.stream():
+    st.write(f"{doc.id} has scored: {doc.to_dict()} points")
 
 # # Create new test_score document
 # doc_ref = db.collection("test_scores").document("player3_scores")
@@ -28,21 +37,3 @@ for doc in doc_ref.stream():
 #     "rider3_mgp": "Messi",
 #     "bonus30_mgp": 0
 # })
-
-players = ["Dave", "Frederic"]
-st.write("## Setting test")
-player_data = db.collection("Dave")
-doc_ref = player_data.list_documents()
-for doc in doc_ref:
-    st.write("Getting collections")
-    collections = doc.collections()
-    st.write("Now looping through collections")
-    for collection in collections:
-        st.write(f"collection id: {collection.id}")
-        for collection_doc in collection.stream():
-            st.write(f"{collection_doc.id} => {collection_doc.to_dict()}")
-    st.write("Ended collections, now looking at player scores")
-    st.write(f"doc data: {doc.id}")
-    data = doc.get()
-    st.write(f"current player scores for each chosen rider: {data.to_dict()}")
-
