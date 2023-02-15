32ee62687c4425ab0f945a22419b46decb7a393b
ftaylor101 <45371775+ftaylor101@users.noreply.github.com>
2023-02-15 18:36:35 +0000

    Making plotting optional and adding filter to plot

--- score_summary_page.py
+++ score_summary_page.py
@@ -41,43 +41,48 @@ cumulative_plot_df = plot_df.cumsum(axis=1)
 df = cumulative_plot_df.stack().reset_index()
 df.rename(columns={"level_0": "Names", "level_1": "Event", 0: "Score"}, inplace=True)
 
-# altair plotting
-# Create a selection that chooses the nearest point & selects based on x-value
-nearest = alt.selection(type='single', nearest=True, on='mouseover',
-                        fields=['Event'], empty='none')
-# The basic line
-line = alt.Chart(df).mark_line(
-    point=alt.OverlayMarkDef()
-).encode(
-    x='Event:N',
-    y='Score:Q',
-    color='Names:N'
-)
-# Transparent selectors across the chart. This is what tells us the x-value of the cursor
-selectors = alt.Chart(df).mark_point().encode(
-    x='Event:N',
-    opacity=alt.value(0),
-).add_selection(
-    nearest
-)
-# Draw points on the line, and highlight based on selection
-points = line.mark_point().encode(
-    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
-)
-# Draw text labels near the points, and highlight based on selection
-text = line.mark_text(align='left', dx=5, dy=-5).encode(
-    text=alt.condition(nearest, 'Score:Q', alt.value(' '))
-)
-# Draw a rule at the location of the selection
-rules = alt.Chart(df).mark_rule(color='gray').encode(
-    x='Event:N',
-).transform_filter(
-    nearest
-)
-# Put the five layers into a chart and bind the data
-cht = alt.layer(
-    line, selectors, points, rules, text
-).properties(
-    width=600, height=300
-)
-st.altair_chart(cht, use_container_width=True)
+# altair plotting if wanted
+plot_checkbox = st.checkbox("Plot points")
+if plot_checkbox:
+    player_names = list(df["Names"].unique())
+    people_to_plot = st.multiselect("Select players:", player_names, default=player_names)
+    filtered_df = df[df["Names"].isin(people_to_plot)]
+    # Create a selection that chooses the nearest point & selects based on x-value
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['Event'], empty='none')
+    # The basic line
+    line = alt.Chart(filtered_df).mark_line(
+        point=alt.OverlayMarkDef()
+    ).encode(
+        x='Event:N',
+        y='Score:Q',
+        color='Names:N'
+    )
+    # Transparent selectors across the chart. This is what tells us the x-value of the cursor
+    selectors = alt.Chart(filtered_df).mark_point().encode(
+        x='Event:N',
+        opacity=alt.value(0),
+    ).add_selection(
+        nearest
+    )
+    # Draw points on the line, and highlight based on selection
+    points = line.mark_point().encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+    # Draw text labels near the points, and highlight based on selection
+    text = line.mark_text(align='left', dx=5, dy=-5).encode(
+        text=alt.condition(nearest, 'Score:Q', alt.value(' '))
+    )
+    # Draw a rule at the location of the selection
+    rules = alt.Chart(filtered_df).mark_rule(color='gray').encode(
+        x='Event:N',
+    ).transform_filter(
+        nearest
+    )
+    # Put the five layers into a chart and bind the data
+    cht = alt.layer(
+        line, selectors, points, rules, text
+    ).properties(
+        width=600, height=300
+    )
+    st.altair_chart(cht, use_container_width=True)
